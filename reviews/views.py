a13e737b3bd928222d0dc5cc68f514389c253f2a
woocheolll <woocheol2868@gmail.com>
2022-10-27 14:13:12 +0900

    reviews/create

--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Review
+from .forms import ReviewForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -19,3 +21,16 @@ def detail(request, pk):
         'review':review,
     }
     return render(request, 'reviews/detail.html',context)
+
+@login_required
+def create(request):
+    if request.method == 'POST':
+        review_form = ReviewForm(request.POST)
+        if review_form.is_valid():
+            db_review_form = review_form.save(commit=False)
+            db_review_form.user = request.user
+            db_review_form.save()
+            return redirect('reviews:index')
+    else:
+        review_form = ReviewForm()
+    return render(request,'reviews/create.html',{'review_form':review_form})
